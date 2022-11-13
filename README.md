71f0446da204331efccc84ed6b90888fb63a2946
saprunovdev <saprunovdev@gmail.com>
2022-11-13 21:18:59 +0900

    completed functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 30 Days of JavaScript challange
 ---
 https://github.com/Asabeneh/30-Days-Of-JavaScript
+
+- [ ] Soleve HW from day 7 (funcitons)
---
+++ day7/script.js
@@ -0,0 +1,39 @@
+// Declare a function fullName and it print out your full name.
+// function fullName(){
+//     console.log('Saprunov Vadim')
+// }
+// fullName()
+
+// Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+// function fullName(firstName, lastName){
+//     return firstName + " " + lastName
+// }
+
+// console.log(fullName("Vadim", "Saprunov"))
+
+// Declare a function addNumbers and it takes two two parameters and it returns sum.
+
+// function addNumbers(num1, num2){
+//     return num1 + num2
+// }
+
+// console.log(addNumbers(3,4))
+
+// An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+
+// A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+
+// A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+
+// Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
+
+// Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+
+// Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+
+// Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+
+// Weight of a substance is calculated as follows: weight = mass x gravity. Write a function which calculates weight.
+
+// Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
+
