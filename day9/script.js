64aaaa4cb1ad599a4498931dcd24061cf4a82b5e
saprunovdev <saprunovdev@gmail.com>
2022-11-15 16:26:56 +0900

    day9 continue

--- day9/script.js
+++ day9/script.js
@@ -1,3 +1,59 @@
+// Functional programming
+
+//forEach
+//forEach iterates through an array
+//it takes element, index and array itself as a parametr
+
+arr = [1,2,8,9,10,4,5,6,7]
+
+//forEach function takes arrow function as an argument
+arr.forEach(e => console.log(e))
+
+//Map takes array as an argument with a calllback function and returns new array
+
+//map iterates through the arr array and returns square numbers as a result
+newArr = arr.map(e => e**2)
+console.log(newArr)
+
+//Filter returns new array with elements, which matches the callback function condition
+
+// filteredArr = arr.filter(e => e >= 3)
+// console.log(filteredArr)
+
+//Reduce iterates through an array and return single value
+
+sumArr = arr.reduce((acc, cur) => acc + cur, 0)
+console.log(sumArr)
+
+//Every checks the types of data in array and returns boolean
+//Compares each element of the array with the condition we provide
+isNumbersArray = arr.every(e => typeof e  === typeof 1)
+console.log(isNumbersArray)
+
+//Find returns the first element which satisfies the condition
+
+greater5 = arr.find(e => e > 5)
+console.log(greater5)
+
+//findIndex returns the index of the first element which satisfies the condition
+
+greater6 = arr.findIndex(e => e > 6)
+console.log(greater6)
+
+//some checks if some of the elements are matchin the callback function condition
+
+someArr = arr.some(e => e >= 10)
+console.log(someArr)
+
+//sort method is sorting elements in the array by descending or accending order.
+//sort uses elements as a string to compare to each other
+//hence, to sort numbers array we have to provide a callback function to compare elements.
+sortedArr = arr.sort((a, b) => a-b)
+console.log(sortedArr)
+
+
+//Level 1
+
 const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -11,75 +67,25 @@ const products = [
 ]
 
 // Explain the difference between forEach, map, filter, and reduce.
-// Instead of writing new loops each time we can use this functions to iterate through given array
-// And pass functions to this functions to fulfill our needs.
+//
 
 // Define a callback function before you use it in forEach, map, filter or reduce.
-e = (e) => console.log(e)
 // Use forEach to console.log each country in the countries array.
-countries.forEach(e)
 // Use forEach to console.log each name in the names array.
-names.forEach(e)
 // Use forEach to console.log each number in the numbers array.
-numbers.forEach(e)
 // Use map to create a new array by changing each country to uppercase in the countries array.
-const uppercaseCountries = countries.map((country) => country.toUpperCase())
-console.log(uppercaseCountries)
-
 // Use map to create an array of countries length from countries array.
-const countriesLength = countries.map((country) => country.length)
-console.log(countriesLength)
 // Use map to create a new array by changing each number to square in the numbers array
-const squareNumbers = numbers.map((num) => num**2)
-console.log(squareNumbers)
-
 // Use map to change to each name to uppercase in the names array
-const uppercaseNames = names.map((name) => name.toUpperCase())
-console.log(uppercaseNames)
-
 // Use map to map the products array to its corresponding prices.
-
 // Use filter to filter out countries containing land.
-const landCountries = countries.filter((country) => country.toLowerCase().includes('land'))
-console.log(landCountries)
-
 // Use filter to filter out countries having six character.
-const sixCharCountries = countries.filter(
-    (country) => country.length >= 6
-)
-
-console.log(sixCharCountries)
 // Use filter to filter out countries containing six letters and more in the country array.
-
-
 // Use filter to filter out country start with 'E';
-const countriesStartingWithE = countries.filter(
-    (country) => country.toLocaleLowerCase().startsWith('e')
-)
-
-console.log(countriesStartingWithE)
 // Use filter to filter out only prices with values.
-// const onlyValues = products.filter(
-//     (product) => product.key
-// )
-
-// console.log(onlyValues)
-
-// Declare a function called getStringLists 
-//which takes an array as a parameter 
-//and then returns an array only with string items.
-
-
+// Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
 // Use reduce to sum all the numbers in the numbers array.
-
-const sum = numbers.reduce(
-    (num) => num
-)
-console.log(sum)
-// Use reduce to concatenate all the countries and to produce this sentence: 
-//Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
-
-
+// Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
 // Explain the difference between some and every
 // Use some to check if some names' length greater than seven in names array
 // Use every to check if all the countries contain the word land
