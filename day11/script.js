dd2dd1bc53e6d2c797ffa9c1b537df724a26e993
saprunovdev <saprunovdev@gmail.com>
2022-11-17 17:01:07 +0900

    finished day 11

--- day11/script.js
+++ day11/script.js
@@ -2,7 +2,42 @@
 
 //Destructing is a way to unpack values and store them to the distinct values
 
-const nums = [1,2,3]
-let [first, second, third] = nums
+//Exercises
 
-console.log(third)
+// Destructure and assign the elements of constants array 
+//to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+
+const [e, pi, gravity, humanBodyTemp] = constants
+console.log(e, humanBodyTemp)
+
+// Destructure and assign the elements of countries array to fin, est, sw, den, nor
+const[fin, est, sw, den, nor] = countries
+console.log(nor, est)
+
+// Destructure the rectangle object by its properties or keys.
+const {width: w, height: h, area: a, perimeter: p} = rectangle
+console.log(w, p)
+
+const [...keys] = Object.keys(rectangle)
+
+console.log(keys)
+// Iterate through the users array and get all the keys of the object using destructuring
+// let userKeys = []
+// for( let i of users){
+//     let keys = {...Object.keys(i)}
+//     console.log(keys)
+// }
+
+// Find the persons who have less than two skills
+
+let noobs = users.filter(e => e.skills.length <= 2? e: null)
+console.log(noobs)
+
+//Destructure the countries object print 
+//name, capital, population and languages of all countries
+for (const{name:n, capital:c, population: p, languages: l} of countries){
+    console.log(`Name: ${n}\nCapital: ${c}\nPopulation: ${p}\nLanguages${l}\n`)
+}
+
+
+
--- threejs_practice/js/main.js
+++ threejs_practice/js/main.js
@@ -1,24 +1,31 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-const cube = new THREE.Mesh( geometry, material );
+// add cube
+var geometry = new THREE.BoxGeometry( 20, 20, 20);
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+// rotate cube
+cube.rotation.x = 0.45;
+cube.rotation.y = -0.25;
 
-function animate() {
-    requestAnimationFrame( animate );
+camera.position.z = 100;
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+var light = new THREE.PointLight( 0xFFFF00 );
+light.position.set( 10, 0, 25 );
+scene.add( light );
 
-    renderer.render( scene, camera );
+
+var render = function () {
+  requestAnimationFrame( render );
+
+  renderer.render(scene, camera);
 };
 
-animate();
+render();
