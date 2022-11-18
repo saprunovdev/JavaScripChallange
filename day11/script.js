e405a1ba8a69f2c5d8d285aa337755598427c587
saprunovdev <saprunovdev@gmail.com>
2022-11-18 10:59:00 +0900

    completed day 11

--- day11/script.js
+++ day11/script.js
@@ -40,4 +40,51 @@ for (const{name:n, capital:c, population: p, languages: l} of countries){
 }
 
 
+const students = [
+    ['David', ['HTML', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTML', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+  ]
 
+function convertArrayToObject(students){
+    let studentsArray = []
+    for (let i in students){
+        console.log(students[i])
+        const[name, skills, scores] = students[i]
+
+        const obj = {
+        name: name,
+        skills: skills,
+        scores: scores
+        }
+
+    studentsArray.push(obj)
+    }
+    return studentsArray
+}
+
+console.log(convertArrayToObject(students))
+
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+  }
+
+const copyStudent = {...student, frontEnd:[{skill:'SQL', level: 8}]}
+
+console.log(copyStudent)
