7c9e3f5ccc7241166ad37fbd443d2ef5e3fa3f6f
saprunovdev <saprunovdev@gmail.com>
2022-11-22 14:55:54 +0900

    learned add event listener

---
+++ day23/script.js
@@ -0,0 +1,23 @@
+//take num as input and generate that much numbers in the div below
+const input = document.querySelector('input')
+const numbers = document.querySelector('.numbers')
+
+const generate = () => {
+    let num
+    numbers.innerHTML = ''
+    for(let i = 0; i < input.value; i++){
+        num = document.createElement('p')
+        num.style.display = "inline-block"
+        num.style.padding = "15px"
+        num.textContent = i
+        i % 2 == 0? num.style.color = 'green' : num.style.color = 'red';
+        numbers.appendChild(num)
+    }
+}
+
+const keyDisplay = document.querySelector('#current-key')
+
+addEventListener('keydown', e => {
+    keyDisplay.innerHTML = ''
+    keyDisplay.textContent = e.keys
+})
